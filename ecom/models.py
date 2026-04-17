c58990fb2077b2acf741f2a59cf36f4cccdb817c
Ashish Pise <aspise1817@gmail.com>
2026-04-18 00:10:12 +0530

    Sync local changes

--- ecom/models.py
+++ ecom/models.py
@@ -4,79 +4,46 @@ from django.db import models
 
 class Student(models.Model):
     email = models.EmailField()
-    name = models.CharField(max_length=50)
+    name = models.CharField()
+
 
     def __str__(self):
-        return self.name
+        return self.email
     
-
-
+    
 class Img(models.Model):
     name = models.CharField(max_length=50)
     image = models.ImageField(upload_to='test_imgs')
-
-    def __str__(self):
-        return self.name
     
 
+    def __str__(self):
+        return self.name    
+    
 class Registration(models.Model):
-    email = models.EmailField()
     name = models.CharField(max_length=50)
+    email = models.EmailField()
     mob = models.CharField(max_length=10)
-    add = models.TextField(default="")
-    password = models.CharField(max_length=8)
+    password = models.CharField(max_length=10)
+    address = models.TextField()
 
     def __str__(self):
         return self.name
     
-
-class category(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='cat_img')
-    discription = models.TextField()
-    
+    image = models.ImageField(upload_to='cat_imgs')
+    description = models.TextField()
+
     def __str__(self):
         return self.name
     
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='cat_img')
-    discription = models.TextField()
-    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='prod_imgs')
     price = models.PositiveIntegerField()
-    category = models.ForeignKey(category,on_delete=models.CASCADE)
+    stock = models.PositiveIntegerField()
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-    
-
-
-class Order(models.Model):
-    user = models.ForeignKey(Registration,on_delete=models.CASCADE)
-    pro = models.ForeignKey(Product,on_delete=models.CASCADE)
-    qty = models.PositiveIntegerField()
-    name = models.CharField(max_length=50)
-    mob = models.CharField(max_length=15)
-    add = models.TextField()
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    pin = models.CharField(max_length=10,default="12")
-    total_price = models.PositiveIntegerField()
-    payment_type = models.CharField(max_length=50)
-    payment_id = models.CharField(max_length=100)
-    dt = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return str(self.user)
-    
-
-
-class Cart(models.Model):
-    user = models.ForeignKey(Registration,on_delete=models.CASCADE)
-    pro = models.ForeignKey(Product,on_delete=models.CASCADE)
-    qty = models.PositiveIntegerField()
-    total_price = models.PositiveIntegerField()
-    order_id = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        return str(self.user)
--- vendor/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path , include
-from .views import *
-
-urlpatterns = [
-   
-    path('ven_reg/',registaration_ven,name='ven_reg')
-
-]  
